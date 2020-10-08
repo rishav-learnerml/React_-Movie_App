3e176f0bd9d8ae411316b2f56085e0ad2dde4a13
rishav-learnerml <rishavbio1@gmail.com>
2020-10-08 09:31:47 +0530

    all files added

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,23 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
+import Header from "./components/Header/Header";
+import Movies from "./components/Header/Movies";
 
-function App() {
+type Movie = {
+  imdbID: string
+  title: string
+  image: string
+  year: string
+}
+
+const App: React.FC = () => {
+  const [movies, setMovies] = useState<Movie[]>([]);
+  const [tempMovies, setTempMovies] = useState<Movie[]>([]);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header movies={movies} setMovies={setTempMovies}/>
+      <Movies movies={tempMovies} setMovies={setMovies} setTempMovies={setTempMovies}/>
+      
     </div>
   );
 }
---
+++ src/components/Header/Movies/index.tsx
@@ -0,0 +1,72 @@
+import React, { useEffect, useState } from "react";
+import Movie from "../Movie";
+import CircularProgress from "@material-ui/core/CircularProgress";
+import "./style.css";
+
+type Props = {
+  movies: any;
+  setMovies: any;
+  setTempMovies: any;
+};
+
+type Movie ={
+  imdbID: string
+  title: string
+  image: string
+  year: string
+}
+
+const API_KEY = "eb7f19c3";
+
+const series = ["avengers", "fast and furious", "iron man", "harry potter"];
+
+const Movies: React.FC<Props> = (props) => {
+  useEffect(() => {
+    const promises = series.map((series) => {
+      return fetch(
+        `http://www.omdbapi.com/?s=${encodeURIComponent(
+          series
+        )}&apikey=${API_KEY}&page=1`
+      ).then((result) => result.json());
+    });
+
+    Promise.all(promises).then((movies: any) => {
+      const updatedMovies: Movie[] = movies.map((movie: any) => movie.Search).flat(2).map(
+        (movie: any) => ({
+          title: movie.Title,
+          year: movie.Year,
+          image: movie.Poster,
+          imdb: movie.imdbID
+
+        }))
+
+      props.setMovies(updatedMovies)
+      props.setTempMovies(updatedMovies)
+    })
+  }, [])
+
+  if (props.movies.length === 0) {
+    return (
+      <div className="loader">
+        <CircularProgress />
+      </div>
+    );
+  }
+
+  return (
+    <div className="movies">
+      {props.movies.map((movie: Movie) => {
+        return (
+          <Movie
+            key={movie.imdbID}
+            title={movie.title}
+            year={movie.year}
+            image={movie.image}
+          />
+        );
+      })}
+    </div>
+  );
+};
+
+export default Movies;
