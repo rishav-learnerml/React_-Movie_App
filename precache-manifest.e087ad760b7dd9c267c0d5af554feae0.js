a1909bfd3b3752727e8566d4060b2b1e3706da11
rishav-learnerml <rishavbio1@gmail.com>
2020-10-08 10:04:50 +0530

    Updates

---
+++ precache-manifest.e087ad760b7dd9c267c0d5af554feae0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3bc3d24ac112ce1277227b9e2494ca3d",
+    "url": "/React_-Movie_App/index.html"
+  },
+  {
+    "revision": "5dc5b521e065c847bd19",
+    "url": "/React_-Movie_App/static/css/main.1e7dca56.chunk.css"
+  },
+  {
+    "revision": "00eb85d45fc7593169da",
+    "url": "/React_-Movie_App/static/js/2.8dbd3308.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/React_-Movie_App/static/js/2.8dbd3308.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5dc5b521e065c847bd19",
+    "url": "/React_-Movie_App/static/js/main.281de890.chunk.js"
+  },
+  {
+    "revision": "c798521c12b70a744b3d",
+    "url": "/React_-Movie_App/static/js/runtime-main.51efd073.js"
+  }
+]);
